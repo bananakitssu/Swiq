ccdc31454c15b7417e3aeb83a8a6004f57820b23
bananakitssu <bananacool467+2@gmail.com>
2026-06-16 01:56:51 -0400

    website

---
+++ build/CMakeFiles/swiq.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/data/data/com.termux/files/home/swiq/src/interpreter.cpp" "CMakeFiles/swiq.dir/src/interpreter.cpp.o" "gcc" "CMakeFiles/swiq.dir/src/interpreter.cpp.o.d"
+  "/data/data/com.termux/files/home/swiq/src/lexer.cpp" "CMakeFiles/swiq.dir/src/lexer.cpp.o" "gcc" "CMakeFiles/swiq.dir/src/lexer.cpp.o.d"
+  "/data/data/com.termux/files/home/swiq/src/main.cpp" "CMakeFiles/swiq.dir/src/main.cpp.o" "gcc" "CMakeFiles/swiq.dir/src/main.cpp.o.d"
+  "/data/data/com.termux/files/home/swiq/src/parser.cpp" "CMakeFiles/swiq.dir/src/parser.cpp.o" "gcc" "CMakeFiles/swiq.dir/src/parser.cpp.o.d"
+  "" "swiq" "gcc" "CMakeFiles/swiq.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/swiq.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/swiq.dir/link.d"
+  "CMakeFiles/swiq.dir/src/interpreter.cpp.o"
+  "CMakeFiles/swiq.dir/src/interpreter.cpp.o.d"
+  "CMakeFiles/swiq.dir/src/lexer.cpp.o"
+  "CMakeFiles/swiq.dir/src/lexer.cpp.o.d"
+  "CMakeFiles/swiq.dir/src/main.cpp.o"
+  "CMakeFiles/swiq.dir/src/main.cpp.o.d"
+  "CMakeFiles/swiq.dir/src/parser.cpp.o"
+  "CMakeFiles/swiq.dir/src/parser.cpp.o.d"
+  "swiq"
+  "swiq.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/swiq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
