cf181a5da4038be8f7472ed14864eea75d104a16
bananakitssu <bananacool467+2@gmail.com>
2026-06-18 20:31:26 -0400

    Update

--- new.tsx
+++ new.tsx
@@ -1,5 +1,13 @@
+import { createRoot } from 'react-dom/client';
+
 export default function SwiqHome() {
     return (
         <p>hi</p>
     )
+}
+
+const container = document.getElementById('root');
+if (container) {
+    const root = createRoot(container);
+    root.render(<SwiqHome />);
 }
