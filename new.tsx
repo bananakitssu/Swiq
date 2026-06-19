ab2e853610058d89082d23e9939b2451f1451260
bananakitssu <bananacool467+2@gmail.com>
2026-06-18 22:57:58 -0400

    update

--- new.tsx
+++ new.tsx
@@ -8,7 +8,7 @@ export default function SwiqHome() {
                 position: 'relative',
                 top: 0,
                 left: 0,
-                width: '100%'
+                width: '100%',
                 height: '30px'
             }}></nav>
         </div>
